54ca219ecc4c9befb35ff6d2665ac4f1e9a35143
XiaoNing-Bu <94035865+XiaoNing-Bu@users.noreply.github.com>
2023-10-22 12:41:24 -0400

    add a dummy controller

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+"""
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -14,3 +14,25 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+from fastapi import FastAPI, Response
+
+# I like to launch directly and not use the standard FastAPI startup
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Awesome cloud developer xb2171 says hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8012)
